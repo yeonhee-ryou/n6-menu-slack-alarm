588689d6715680966c11cbb42ed445f4027f17a0
Yeonhee Ryou <yeonhee1ryou@gmail.com>
2023-01-12 15:35:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import requests
 
 from bs4 import BeautifulSoup as bs
@@ -7,13 +8,25 @@ from datetime import datetime
 from slack_sdk import WebClient, errors
 
 
+RET_NAMES = {
+    "교수회관": "emp",
+    "카이마루": "fclt"
+}
+
+
 def url(name, y, m, d):
     return f"https://www.kaist.ac.kr/kr/html/campus/053001.html?dvs_cd={name}&stt_dt={y}-{m:0>2}-{d:0>2}"
 
 
 def get_data():
+    menu = [get_menu(name) for name in RET_NAMES]
+    return "\n".join(menu)
+
+
+def get_menu(name):
     t = datetime.now()
-    req = requests.get(url("emp", t.year, t.month, t.day))
+    endpoint = url(RET_NAMES[name], t.year, t.month, t.day)
+    req = requests.get(endpoint)
     
     soup = bs(req.text, "html.parser")
     table = soup.find("table")
@@ -27,12 +40,18 @@ def get_data():
         rows.append(row)
     
     msg = ""
+    skip_p = re.compile("\d+")
     for row in rows:
         for key, value in row.items():
-            if "조식" not in key:
-                msg += f"\n{key}\n"
+            if "중식" in key:
+                msg += f"* <{endpoint}|{name}> {key}\n"
                 for line in value.splitlines():
-                    msg += f"> {line.strip()}\n"
+                    sline = line.strip()
+                    if sline.lower() == "kcal":
+                        continue
+                    if skip_p.match(sline):
+                        continue
+                    msg += f"> {sline}\n"
     return msg.strip()
 
 
